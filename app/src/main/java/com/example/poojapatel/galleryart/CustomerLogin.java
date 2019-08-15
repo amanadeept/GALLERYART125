d5a54f220594878d3c3513d63a8491f5e1991879
Aman <mann.aman12@gmail.com>
2019-08-15 15:46:47 -0400

    init

--- app/src/main/java/com/example/poojapatel/galleryart/CustomerLogin.java
+++ app/src/main/java/com/example/poojapatel/galleryart/CustomerLogin.java
@@ -2,140 +2,93 @@ package com.example.poojapatel.galleryart;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Patterns;
+import android.app.Activity;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import org.json.JSONException;
-import org.json.JSONObject;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
-import java.net.URL; import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
 
-public class Login extends AppCompatActivity {
-    private EditText email_et,password_et;
-    //String username;
+import com.google.android.gms.tasks.OnCompleteListener;
+
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class CustomerLogin extends Activity {
+    private Button bb2, button, bu1;
+    private EditText email, password;
+    private FirebaseAuth Auth;
     private ProgressDialog pd;
-    String email,password;
+    private CheckBox c1;
+
+
+    // button_login
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_login);
-        email_et=findViewById(R.id.input_username);
-        password_et=findViewById(R.id.input_password);
-        pd = new ProgressDialog(this);
-        pd.setMessage("please wait....");
-        pd.setTitle("loading");
-    }
 
-    public void do_login(View view){
-        email=email_et.getText().toString();
-        password=password_et.getText().toString();
-        if (Patterns.EMAIL_ADDRESS.matcher(email).matches())
-        {
-            Toast.makeText(Login.this,"Please check your email",Toast.LENGTH_SHORT).show();
-            return;
-        }
-        if (password.length()<8)
-        {
-            Toast.makeText(Login.this,"Password atleast 8 characters",Toast.LENGTH_SHORT).show();
-            return;
-        }
+//        pd.setCanceledOnTouchOutside(false);
+        Auth = FirebaseAuth.getInstance();
 
-        new MyTask().execute();
+//if (Auth.getCurrentUser()!=null){
+//    Intent intent=new Intent(getApplicationContext(),Main2Activity.class);
+//    startActivity(intent);
+//    finish();
+//}
+        email= findViewById(R.id.input_username);
+        bb2 = (Button) findViewById(R.id.button_login);
+        button = (Button) findViewById(R.id.button_register);
+        bu1 = (Button) findViewById(R.id.button_forgot_password);
+        c1 = findViewById(R.id.checkbox);
+        password = findViewById(R.id.input_password);
 
-    }
-    public void signup(View view){
 
-        Intent i =getIntent();
-        Bundle bundle=i.getExtras();
-        email=bundle.getString("type");
-        Intent in=new Intent(Login.this,Home.class);
-        in.putExtra("type",email);
-        startActivity(in);
-    }
-    private class MyTask extends AsyncTask<Void,Void,Void> {
-        int fulluid;
-        String user_status;
 
-        public String getUser_status() {
-            return user_status;
-        }
 
-        public void setUser_status(String user_status) {
-            this.user_status = user_status;
-        }
 
-        public int getFulluid() {
-            return fulluid;
-        }
 
-        public void setFulluid(int fulluid) {
-            this.fulluid = fulluid;
-        }
 
-        @Override
-        protected Void doInBackground(Void... params) {
-            URL url = null;
-            try {
-                url = new URL("http://localhost:8080/WebApplication5/webresources/mobile/Login&" + email + "&" + password);
-                HttpURLConnection client = null;
-                client = (HttpURLConnection) url.openConnection();
-                client.setRequestMethod("GET");
-                int resonseCode = client.getResponseCode();
-                System.out.println("\n Sending 'GET' request to URL : " + url);
-                System.out.println("Response code :" + resonseCode);
-                InputStreamReader myInput = new InputStreamReader(client.getInputStream());
-                BufferedReader in = new BufferedReader(myInput);
-                String inputLine;
-                StringBuffer response = new StringBuffer();
+        bb2.setOnClickListener(new View.OnClickListener() {
+                                   @Override
+                                   public void onClick(View v) {
 
-                while ((inputLine = in.readLine()) != null) {
-                    response.append(inputLine);
-                }
-                in.close();
-                System.out.println(response.toString());
-                JSONObject object = new JSONObject(response.toString());
+                                       Intent intent = new Intent(getApplicationContext(), HomePage.class);
+                                       startActivity(intent);
+                                       finish();
+                                   }
 
-                user_status = object.getString("status");
-                setUser_status(user_status);
-                int userid = object.getInt("USER_ID");
-                Da datamodel=Datamodel.geInstance();
-                datamodel.setUser_id(userid);
+                               });
 
-                datamodel.setEmail(object.getLong("EMAIL_ID"));
-
-            }catch (MalformedURLException e){
-                e.printStackTrace();
-            }catch (ProtocolException e){
-                e.printStackTrace();
-            }catch (IOException e){
-                e.printStackTrace();
-            }catch (JSONException e){
-                e.printStackTrace();
+        bu1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent1 = new Intent(getApplicationContext(),forgetpassword.class);
+                startActivity(intent1);
             }
-            return null;
-        }
-        @Override
-        protected void onPostExecute(Void result){
-            super.onPostExecute(result);
-            System.out.println("pyy"+getUser_status()+email);
-            if (getUser_status().equals("ok")) {
-                Intent bridge = new Intent(getApplicationContext(), HomePage.class);
-                Toast.makeText(getApplicationContext(), "You are successfully", Toast.LENGTH_SHORT).show();
-                startActivity(bridge);
-            }else {
-                Toast.makeText(getApplicationContext(),"please recheck your email and password",Toast.LENGTH_LONG).show();
+        });
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SignUpPage.class);
+
+                startActivity(intent);
+
+
             }
 
-        }
-    }}
+
+        });
+
+    }
+}
--- app/src/main/java/com/example/poojapatel/galleryart/CustomerRegistration.java
+++ app/src/main/java/com/example/poojapatel/galleryart/CustomerRegistration.java
@@ -1,48 +1,36 @@
 package com.example.poojapatel.galleryart;
+
+import android.app.ProgressDialog;
 import android.content.Intent;
-import android.os.AsyncTask;
-//import android.support.v7.app.AppCompatActivity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.service.autofill.UserData;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.RadioButton;
+
 import android.widget.RadioGroup;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
-import java.net.URL;
-//        import android.content.Intent;
-//
-     import androidx.appcompat.app.AppCompatActivity;
-//
-//        import android.os.Bundle;
-//        import android.view.Menu;
-//        import android.view.View;
-//        import android.widget.Button;
-//        import android.widget.CheckBox;
-//        import android.widget.EditText;
-//        import android.widget.RadioButton;
-//
-//        import android.widget.RadioGroup;
-//        import android.widget.Toast;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-
+import java.util.HashMap;
 
 public class CustomerRegistration extends AppCompatActivity {
 
     private EditText Fn, Ln, email, address, password, confpass, contact;
-
-
     private RadioButton r1, r2;
     private CheckBox r3;
     private Button bb1;
@@ -52,14 +40,14 @@ public class CustomerRegistration extends AppCompatActivity {
     RadioGroup rg1;
     RadioGroup rg2;
 
-//    EditText t,t1, t2, t3, t4, t5;
-//    String type, username, firstname, password, lastname, email;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.regiscustomer);
+
+
+        setContentView(R.layout.activity_sign_up_page);
+
 
         Fn = findViewById(R.id.input_fullName);
         Ln = findViewById(R.id.input_lastName);
@@ -80,6 +68,7 @@ public class CustomerRegistration extends AppCompatActivity {
         bb1 = findViewById(R.id.button_register_singnupPage);
 
 
+
         bb1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -89,10 +78,21 @@ public class CustomerRegistration extends AppCompatActivity {
 
         });
 
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+
 
     }
 
-        private void submitForm() {
+
+    private void submitForm() {
 
 
         int sexRadio = rg1.getCheckedRadioButtonId();
@@ -106,54 +106,64 @@ public class CustomerRegistration extends AppCompatActivity {
             sex = "Male";
         }
 
+        int customerRadio = rg2.getCheckedRadioButtonId();
 
+        // If nothing is selected from Radio Group, then it return -1
+        if (customerRadio != -1) {
 
-
-        if (Fn.getText().toString().isEmpty()) {
-            Fn.setError("First name not entered");
-
-            Fn.requestFocus();
-
-            return;
-        }
-        if (Ln.getText().toString().isEmpty()) {
-            Ln.setError("Last name not entered");
-            Ln.requestFocus();
-            return;
-
+            RadioButton selectedRadioButton = (RadioButton) findViewById(customerRadio);
+            user = selectedRadioButton.getText().toString();
+        } else {
+            user = "Customer";
         }
 
-        if (address.getText().toString().isEmpty()) {
-            address.setError("Address is Required");
-            address.requestFocus();
-            return;
-        }
-        if (password.getText().toString().isEmpty()) {
-            password.setError("Password not entered");
-            password.requestFocus();
-            return;
-        }
-        if (contact.getText().toString().isEmpty()) {
-            contact.setError("Required");
-            contact.requestFocus();
-            return;
-        }
 
-        if (confpass.getText().toString().isEmpty()) {
-            confpass.setError("Please confirm password");
-            confpass.requestFocus();
-            return;
-        }
-        if (!password.getText().toString().equals(confpass.getText().toString())) {
-            confpass.setError("Password Not matched");
-            confpass.requestFocus();
-
-        }
-        if (password.getText().toString().length() < 8) {
-            password.setError("Password should be atleast of 8 charactors");
-            password.requestFocus();
-
-        }
+        //*******************************************
+//        if (Fn.getText().toString().isEmpty()) {
+//            Fn.setError("First name not entered");
+//
+//            Fn.requestFocus();
+//
+//            return;
+//        }
+//        if (Ln.getText().toString().isEmpty()) {
+//            Ln.setError("Last name not entered");
+//            Ln.requestFocus();
+//            return;
+//
+//        }
+//
+//        if (address.getText().toString().isEmpty()) {
+//            address.setError("Address is Required");
+//            address.requestFocus();
+//            return;
+//        }
+//        if (password.getText().toString().isEmpty()) {
+//            password.setError("Password not entered");
+//            password.requestFocus();
+//            return;
+//        }
+//        if (contact.getText().toString().isEmpty()) {
+//            contact.setError("Required");
+//            contact.requestFocus();
+//            return;
+//        }
+//
+//        if (confpass.getText().toString().isEmpty()) {
+//            confpass.setError("Please confirm password");
+//            confpass.requestFocus();
+//            return;
+//        }
+//        if (!password.getText().toString().equals(confpass.getText().toString())) {
+//            confpass.setError("Password Not matched");
+//            confpass.requestFocus();
+//
+//        }
+//        if (password.getText().toString().length() < 8) {
+//            password.setError("Password should be atleast of 8 charactors");
+//            password.requestFocus();
+//
+//        }
 
         First_Name = Fn.getText().toString().trim();
         Last_Name = Ln.getText().toString();
@@ -176,138 +186,11 @@ public class CustomerRegistration extends AppCompatActivity {
             return;
         }
 
-        Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_LONG).show();
 
 
-        Intent i = new Intent(getApplicationContext(),CustomerLogin.class);
-        startActivity(i);
 
 
-        }
+
+
     }
-
-
-
-
-
-         public void register(View view) {
-//
-//                type=t.getText().toString();
-//                username = t1.getText().toString();
-//                password = t2.getText().toString();
-//                firstname = t3.getText().toString();
-//                lastname = t4.getText().toString();
-//                email = t5.getText().toString();
-                System.out.println("Print something"+ First_Name);
-
-                new CustomerRegistration().MyTask().execute();
-
-
-
-
-
-
-
-
-
-
-
-//
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.main, menu);
-//        return true;
-//
-//
-//    }
-//
-//
-//
-
-    private void MyTask() extends AsyncTask<Void, Void, String> {
-                 {
-
-
-                 @Override
-                 protected String doInBackground(Void... params) {
-                     StringBuffer response = new StringBuffer();
-                     URL url = null;
-
-
-                     try {
-
-                         url = new URL("http://10.0.2.2:27897/WebApplication1/Myapp/main/registration&"+ ""+type+""+"&"+"" + username + "" + "&" + "" + password + "" + "&" + "" + firstname + ""
-                                 + "&" + "" + lastname + "" + "&" + "" + email + "");
-
-                         // url = new URL("http://10.0.2.2:27897/WebApplication1/Myapp/main/login&"+ "'" + uname + "'"+ "&" +"'"+pass+"'");
-              /* url = new URL("http://10.0.2.2:27897/WebApplication1/Myapp/main/registration&101"+"&" + "'"+type+"'"+"&"+"'" + username + "'" + "&" + "'" + password + "'" + "&" + "'" + firstname + "'"
-                      + "&" + "'" + lastname + "'" + "&" + "'" + email + "'");
-*/
-                         //url = new URL("http://10.0.2.2:27897/WebApplication1/Myapp/main/Addoffers_travels&1012"+ "'" +  file.getAbsolutePath()+ "'"+
-                         // "&"+"'"+description+"'"+"&"+"'"+offer+"'"+"&"+"'"+Expiration_date+"'");
-
-
-
-                         // url = new URL("http://10.0.2.2:27897/WebApplication1/Myapp/main/Registration&"+type+"&"+username+"&"+password+"&"+lastname+"&"+firstname+"&"+email+"");
-                         // url = new URL("http://10.0.2.2:27897/WebApplication1/Myapp/main/Registration&"+ "'" +type+ "'"+ "&" + "'" +username+ "'" + "&" + "'" +password+ "'" + "&"
-                         //   + "'" +lastname+ "'" + "&" + "'" +firstname+ "'" + "& "+ "'"+email+"'");
-
-                         HttpURLConnection client = null;
-                         client = (HttpURLConnection) url.openConnection();
-                         client.setRequestMethod("GET");
-                         int responseCode = client.getResponseCode();
-                         System.out.println("\n Sending 'GET' request to URL : " + url);
-                         System.out.println("Response Code : " + responseCode);
-                         InputStreamReader myInput = new InputStreamReader(client.getInputStream());
-                         BufferedReader in = new BufferedReader(myInput);
-                         String inputLine;
-
-
-                         while ((inputLine = in.readLine()) != null) {
-                             response.append(inputLine);
-                         }
-                         in.close();
-
-                         // print result
-                         System.out.println("The response is " + response.toString());
-
-
-                     } catch (ProtocolException e) {
-                         e.printStackTrace();
-                     } catch (MalformedURLException e) {
-                         e.printStackTrace();
-                     } catch (IOException e) {
-                         e.printStackTrace();
-                     } catch (Exception e) {
-                     }
-
-                     return response.toString();
-                 }
-
-                 protected void onPostExecute(String result) {
-
-                     super.onPostExecute(result);
-                     if (result.equalsIgnoreCase("wrong")) {
-                         Toast.makeText(getApplication(), "error occured while registering, please try again", Toast.LENGTH_LONG).show();
-
-                     } else {
-                         Intent launchActivity1 = new Intent(getApplicationContext(),CustomerLogin.class);
-                         startActivity(launchActivity1);
-
-                     }
-                 }
-
-             }
-         }
-
-
-
-
-
-
-
-
-
-             }
-    }
+}
